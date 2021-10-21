15bf9903237bf70fb029acc406f01c152c24b964
Dularazero <86337319+Dularazero@users.noreply.github.com>
2021-10-21 17:49:38 +0530

    Update Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
         if (!audio.url) return
         M.reply('💫 Sending...')
         this.client
-            .sendMessage(M.from, await audio.getBuffer(), MessageType.document/mp.3, {
+            .sendMessage(M.from, await audio.getBuffer(), MessageType.document/audio, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
